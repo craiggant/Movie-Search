633402b593c76a19e785949804eeba0128d20ac7
Craig Gant <craigkgant@gmail.com>
2022-02-28 22:34:57 -0500

    styled useMovieFilter

--- hooks/useMovieFilter.ts
+++ hooks/useMovieFilter.ts
@@ -1,36 +1,38 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { filterMovies } from '../helper/helperFunctions';
 import { Movie, MovieFilter } from '../types';
 
-const useMovieFilter = (movies:Movie[]|null) => {
-  const initialState: MovieFilter = {
+const useMovieFilter = (movies: Movie[] | null) => {
+	const initialState: MovieFilter = {
 		titleSearch: '',
 		genreSearch: '',
 		searchResults: movies
-  };
+	};
 
-  const [filtered, setFiltered] = useState<MovieFilter>(initialState);
+	const [filtered, setFiltered] = useState<MovieFilter>(initialState);
 
-  const filterOnChange = (e: React.ChangeEvent<HTMLInputElement>):void => {
-    const {name, value} = e.target;
+	const filterOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+		const { name, value } = e.target;
 
-    const titleInput = name ==='titleSearch' ? value : filtered.titleSearch;
-    const genreInput = name ==='genreSearch' ? value : filtered.genreSearch;
+		const titleInput =
+			name === 'titleSearch' ? value : filtered.titleSearch;
+		const genreInput =
+			name === 'genreSearch' ? value : filtered.genreSearch;
 
-    const currentlyFilteredMovies = movies && filterMovies(titleInput, genreInput, movies)
+		const currentlyFilteredMovies =
+			movies && filterMovies(titleInput, genreInput, movies);
 
-    setFiltered({
-      ...filtered,
-      [name]: value,
-      searchResults: currentlyFilteredMovies
-    })
-  }
+		setFiltered({
+			...filtered,
+			[name]: value,
+			searchResults: currentlyFilteredMovies
+		});
+	};
 
-  return {
-    filtered,
-    filterOnChange
-  }
-}
+	return {
+		filtered,
+		filterOnChange
+	};
+};
 
-
-export default useMovieFilter
+export default useMovieFilter;
