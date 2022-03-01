1f8d0ca0c599fb323b6f0c6da3ee3a4ea235b1fe
Craig Gant <craigkgant@gmail.com>
2022-02-28 22:35:44 -0500

    removed unneeded imports from custom hooks

--- hooks/useCurrentMovie.ts
+++ hooks/useCurrentMovie.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getMovieById } from '../api/getMovies';
 import { FullMovieInfo } from '../types';
 
--- hooks/useMovieFilter.ts
+++ hooks/useMovieFilter.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { filterMovies } from '../helper/helperFunctions';
 import { Movie, MovieFilter } from '../types';
 
