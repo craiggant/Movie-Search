fee346bdec5881d6054ed1e8b600e82b2d4a30e5
Craig Gant <craigkgant@gmail.com>
2022-02-28 22:31:50 -0500

    styled Input component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,25 +1,26 @@
-import React, {InputHTMLAttributes} from 'react'
-import styles from "../styles/Input.module.css"
+import React, { InputHTMLAttributes } from 'react';
+import styles from '../styles/Input.module.css';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  name: string,
-  label: string,
-};
+	name: string;
+	label: string;
+}
 
 const Input: React.FC<Props> = ({ name, label, ...rest }) => {
 	return (
 		<div className={styles.inputGroup}>
 			<input
 				className={styles.input}
-        id={name}
-        name={name}
-        placeholder={label}
-        {...rest}
+				id={name}
+				name={name}
+				placeholder={label}
+				{...rest}
 			/>
-      <label htmlFor={name} className={styles.label}>{label}</label>
-
+			<label htmlFor={name} className={styles.label}>
+				{label}
+			</label>
 		</div>
 	);
 };
 
-export default Input
+export default Input;
